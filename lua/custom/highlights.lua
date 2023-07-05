a5c6b12669ac962ee454394ce41e717f3c0735e4
Trevor Grieger <tgrieger@salesforce.com>
2023-07-05 14:46:26 -0400

    Remove italic comments

--- lua/custom/highlights.lua
+++ lua/custom/highlights.lua
@@ -4,13 +4,6 @@
 
 local M = {}
 
----@type Base46HLGroupsList
-M.override = {
-  Comment = {
-    italic = true,
-  },
-}
-
 ---@type HLTable
 M.add = {
   NvimTreeOpenedFolderName = { fg = "green", bold = true },
