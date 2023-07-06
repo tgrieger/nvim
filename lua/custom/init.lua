dce98daf5798a6728a6bad0c18ab423e66dd81f7
Trevor Grieger <tgrieger@salesforce.com>
2023-07-05 20:45:17 -0400

    Add highligh on yank

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -5,3 +5,12 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end,
+})
